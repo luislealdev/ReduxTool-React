4ea1cdf1af19aa822187369c582e6925016151d6
luislealdev <luisrrleal@gmail.com>
2022-07-15 13:59:22 -0500

    Select and dispatch info

--- src/store/slices/counter/counterSlice.js
+++ src/store/slices/counter/counterSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  value: 10,
+  counter: 10,
 };
 
 export const counterSlice = createSlice({
@@ -9,11 +9,7 @@ export const counterSlice = createSlice({
   initialState,
   reducers: {
     increment: (state) => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
-      state.value += 1;
+      state.counter += 1;
     },
   },
 });
