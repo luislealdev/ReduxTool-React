8a7cf6aa29356ac3e8a3510d43cd88440c12e9f9
luislealdev <luisrrleal@gmail.com>
2022-07-16 23:13:26 -0500

    Create thunks

--- PokemonApp.jsx
+++ PokemonApp.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { getPokemons } from './src/store/slices/pokemon/thunks';
 
 export const PokemonApp = () => {
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+      dispatch(getPokemons(0));
+    }, [])
+    
+
   return (
     <>
         <h1>PokemonApp</h1>
---
+++ src/store/slices/pokemon/thunks.js
@@ -0,0 +1,11 @@
+export const getPokemons = (page=0) => {
+    return async(dispatch, getState) => {
+        dispatch(firtsLoadingPokemons());
+
+        //TODO: Make petition https
+
+        //Data pokemons
+
+        //Dispatch the setPokemons
+    }
+}
